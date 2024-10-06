1f905047f25bfc0b32f9f4f38587677d4cab99b8
jockovicvukasin <jockovicvukasin@gmail.com>
2024-10-06 11:15:39 +0000

    Add Stats

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css" />
+    <script defer data-domain="nomore.accountants" src="https://stats.businesspress.io/js/script.js"></script>
 
     @livewireStyles
 </head>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-
+    <script defer data-domain="nomore.accountants" src="https://stats.businesspress.io/js/script.js"></script>
 
     @livewireStyles
 </head>
